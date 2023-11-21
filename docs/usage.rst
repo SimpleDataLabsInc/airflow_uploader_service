b883ad2a92cd1bbb39c9e34a17637836b2537123
Ashish Patel <ashishpatel0720@gmail.com>
2023-11-21 14:51:59 +0530

    init

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use prophecy_pybridge in a project::
+
+    import prophecy_pybridge
---
+++ prophecy_pybridge/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for prophecy_pybridge."""
+
+__author__ = """Ashish Patel"""
+__email__ = 'ashish@prophecy.io'
+__version__ = '0.1.0'
